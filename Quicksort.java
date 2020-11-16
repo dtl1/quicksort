546138c15e9947d82ad54af8d5c4910d5f3bfc2c
dtl1 <61438998+dtl1@users.noreply.github.com>
2020-11-16 18:11:42 +0000

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -1,13 +1,116 @@
+import kotlin.Pair;
+
+import java.io.*;
+import java.util.*;
+
+//https://technologyrediscovery.net/javaoo2/sorting.html
+
 public class Quicksort {
 
+    static int swaps;
+
     public static void main(String[] args) {
-        int[] array = new int[4];
-        array[0] = 4;
-        array[1] = 1;
-        array[2] = 3;
-        array[3] = 2;
+        Random rand = new Random();
+
+        ArrayList<int[]> arrays = new ArrayList<>();
+
+        //stoopid
+        int[] dummy = {0};
+
+        for(int i = 1; i < 1000; i ++){
+           int[] array = new int[rand.nextInt(100 - 1 + 1) + 1];
+
+           for(int j = 0; j < array.length - 1; j++){
+
+               //random int from 1 - 100
+               array[j] = rand.nextInt(100 - 1 + 1) + 1;
+           }
+
+           arrays.add(array);
+        }
+
+//        int[] a1 = {4,1,3,2};
+//        int[] a2 = {1,2,3,4};
+//        int[] a3 = {1,1,1,1};
+//        int[] a4 = {4,3,2,1};
+//        int[] a5 = {1,2,4,3};
+//        int[] a6 = {4,1,3,2};
+//
+//        arrays.add(dummy);
+//        arrays.add(a1);
+//        arrays.add(a2);
+//        arrays.add(a3);
+//        arrays.add(a4);
+//        arrays.add(a5);
+//        arrays.add(a6);
+
+
+        double[][] metricTimes = new double[arrays.size()][2];
+
+        int c = 0;
+
+        for(int[] array: arrays){
+            swaps = 0;
+            double timeTaken = timeAndSort(array);
+            metricTimes[c][0] = swaps;
+            metricTimes[c][1] = timeTaken;
+            c++;
+        }
+
+        try {
+            writeTimes(metricTimes);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static void writeTimes(double[][] metricTimes) throws IOException {
+
+
+
+        FileWriter writer = new FileWriter("metricTimes.csv");
+
+        String strVar;
+
+        //skip the first dummy result
+        for (int i = 1; i < metricTimes.length; i++) {
+            strVar = (int) metricTimes[i][0] + "," + metricTimes[i][1] + "\n";
+            writer.append(strVar);
+        }
+
+        writer.flush();
+        writer.close();
+
+
+
+
+    }
+
+
+    static double timeAndSort(int[] array){
+
+        System.out.println(Arrays.toString(array));
+
+        //get starting system time in nanoseconds
+        long startTime = System.nanoTime();
 
         quickSort(array,0,array.length - 1);
+
+        //get ending system time in nanoseconds
+        long endTime = System.nanoTime();
+
+        //get the difference in milliseconds
+        double diffTime = (endTime - startTime) /1e6;
+
+
+        System.out.println(diffTime +" ms");
+        System.out.println(swaps + " swaps");
+        System.out.println("\n");
+
+
+        return diffTime;
     }
 
     /**
@@ -17,39 +120,60 @@ public class Quicksort {
      * @param end ending index
      */
     static void quickSort(int[] array, int start, int end) {
+
         if (start < end) {
-        /* pi is partitioning index, arr[pi] is now
-           at right place */
+
+            //partition index
             int pi = partition(array, start, end);
 
-            quickSort(array, start, pi - 1);  // Before pi
-            quickSort(array, pi + 1, end); // After pi
+            //left of partition index
+            quickSort(array, start, pi - 1);
+
+            //right of partition index
+            quickSort(array, pi + 1, end);
         }
+
     }
 
-
-    /* This function takes last element as pivot, places
-       the pivot element at its correct position in sorted
-        array, and places all smaller (smaller than pivot)
-       to left of pivot and all greater elements to right
-       of pivot */
+    /**
+     * places pivot element at correct position and all smaller elements left of pivot
+     * and all greater elements right of pivot
+     *
+     * @param array array or sub array being sorted
+     * @param start first index of array or sub array
+     * @param end last index of array or sub array
+     * @return partition index
+     */
     static int partition (int[] array, int start, int end) {
-        // pivot (Element to be placed at right position)
-        pivot = arr[high];
 
-        i = (low - 1)  // Index of smaller element
+        // pivot
+        int pivot = array[end];
 
-        for (j = low; j <= high- 1; j++)
-        {
+
+        int i = (start - 1);
+
+        for (int j = start; j <= end - 1; j++) {
             // If current element is smaller than the pivot
-            if (arr[j] < pivot)
-            {
+            if (array[j] < pivot) {
                 i++;    // increment index of smaller element
-                swap arr[i] and arr[j]
+
+                if(array[j] != array[i] ) {
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                    swaps++;
+                }
+
             }
         }
-        swap arr[i + 1] and arr[high])
-        return (i + 1)
+
+        if(array[i+1] != array[end] ) {
+            int temp = array[i + 1];
+            array[i + 1] = array[end];
+            array[end] = temp;
+            swaps++;
+        }
+        return (i + 1);
     }
 
 }
